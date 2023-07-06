f66063b38bf8772555fd4277e65d0cb0d7f0162e
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-07-05 22:51:33 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,6 @@ sleep 2
 
 apt install golang;
 apt install php
-git clone git clone https://github.com/MTplusWebSystem/CheckWeb
+git clone https://github.com/MTplusWebSystem/CheckWeb
 cd CheckWeb
 go run constructor.go
