5fd07ef7dd4acd22355ec279c9ea8969ddf20248
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-07-06 07:42:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,10 +17,24 @@ echo -e "
 "
 sleep 2
 
-apt install golang;
-apt install php
-apt install screen
-git clone https://github.com/MTplusWebSystem/CheckWeb
+
+function show_progress {
+    local progress=$1
+    local length=50
+    local bar=$(printf "%-$((progress * length / 100))s" | tr ' ' '=')
+    printf "\r[%-${length}s] %d%%" "$bar" "$progress"
+}
+
+apt install golang -y > /dev/null
+show_progress 33
+
+apt install php -y > /dev/null
+show_progress 66
+
+apt install screen -y > /dev/null
+show_progress 100
+
+git clone https://github.com/MTplusWebSystem/CheckWeb > /dev/null
 cd CheckWeb
 chmod 777 Web
 mv Web /bin
